2cd78a46d854603acefe4bc73b7e51efb4bea5bd
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2022-03-20 23:11:14 +0700

    belajar dart library

---
+++ example/example.dart
@@ -0,0 +1,14 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello('Robin'));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+  print(customer);
+  print(product);
+  print(category);
+
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library belajar_dart_library;
+
+export 'src/sayHello.dart';
+export 'src/employee.dart' show Category, Customer, Product;
